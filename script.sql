476e96db4eb1d57dc9ccd2c649d75e1063127b6f
e-s-santos <76019182+e-s-santos@users.noreply.github.com>
2021-12-12 12:25:40 -0300

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,4 +1,10 @@
-create table arquivo ( arquivo nvarchar(255),  ) 
+ create database mimosa; 
+ use mimosa; 
 
-
-create table proc (proc int, hostname nvarchar(255)) 
+ create table arquivo (arquivo varchar(255), email varchar(255), verif int);
+ alter table arquivo add tif datetime;
+ alter table arquivo add tii datetime;
+ alter table arquivo add hostname varchar(100);
+ 
+ 
+ create table proc (proc int, hostname nvarchar(100)) ;
